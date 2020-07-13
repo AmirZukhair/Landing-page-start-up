cc59305a7243aa1ced480c60d8231273ad6ed8b8
Amir_Zukhair <AmirZukhair@gmail.com>
2020-07-13 21:17:16 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+
+
+window.onload = function(){
+
+
+
+
+
+
+
+
+	window.onscroll = function(){
+ 
+let fixed = document.querySelector('.header')
+let pos = window.pageYOffset;
+
+    	   if(pos > 0){
+                 fixed.classList.add('fixed');
+
+    	   } else{
+    	   	   fixed.classList.remove('fixed');
+    	   }
+
+   
+};
+
+$('.review-slider').slick({
+ 
+   slidesToShow: 1,
+   slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow.png">',
+   
+   
+    
+ 
+});
+
+
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__list');
+
+
+burger.addEventListener('click', function(){
+   burger.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+
+let anchors = document.querySelectorAll('header a[href*="#"]');
+
+for (anchor of anchors){
+  if(anchor) {
+    anchor.addEventListener('click',function(e){
+      e.preventDefault();
+      anchorId = this.getAttribute('href');
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: 'smooth', block: 'start'
+      })
+    })
+  }
+}
+
+
+
+
+};
+
